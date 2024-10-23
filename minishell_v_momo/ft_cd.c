47c6bd1a79f0bf283175baa0d786189344c9b67d
Mohamed Abdessmad <mabdessm@c2r5p6.42nice.fr>
2024-10-23 03:39:55 +0200

    ft_cd

---
+++ minishell_v_momo/ft_cd.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabdessm <mabdessm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 22:14:03 by mabdessm          #+#    #+#             */
+/*   Updated: 2024/10/23 03:24:21 by mabdessm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell_v_cam/include/minihell.h"
+
+void ft_cd(t_cmd *cmd, t_env *env)
+{
+	char	*directory = NULL;
+	t_env	*current;
+	char	cwd[1024];
+
+    if (cmd->args[1] == NULL)
+    {
+		current = ft_find_key(env, "HOME");
+		if (current && current->value)
+			directory = current->value + 1;
+        else
+        {
+            printf("cd: HOME not set\n");
+            return ;
+        }
+    }
+    else
+	{
+        directory = cmd->args[1];
+	}
+								//error handling works
+	if (chdir(directory) != 0) 	//memory issue when the directory is valid
+	{
+		perror("cd");
+		return ;
+	}
+	if (env)
+	{
+		current = ft_find_key(env, "PWD");
+		if (current && getcwd(cwd, sizeof(cwd)) != NULL)
+		{
+			ft_update_key(env, "OLDPWD", current->value);
+			ft_update_key(env, "PWD", cwd);
+		}
+		else
+		{
+			perror("getcwd");
+			return ;
+		}
+	}
+}
